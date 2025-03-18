bc89bd0c6953b77471a4f24aa805cc1604cff70d
arifur <mailarifur@gmail.com>
2025-03-19 00:13:52 +0600

    initial commit

---
+++ assets/javascripts/discourse/initializers/script.js
@@ -0,0 +1,6 @@
+export default {
+    name: "log",
+    initialize() {
+      console.log('from tmu plugin');
+    },
+  };
---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: TMU Disourse Plugin
+# about: A plugin for TMU users to show their mattress preferrences. 
+# version: 0.0.1
+# authors: Arifur Rahman
+# url: https://github.com/arifur/tmu-discourse-plugin.git
